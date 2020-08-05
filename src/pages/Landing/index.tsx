9422aff53933b823b72e96f1e3754aee76678631
iarademiranda <62669057+iarademiranda@users.noreply.github.com>
2020-08-04 21:21:04 -0300

    PageHeader, TeacherList {css incompleto}

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './styles.css'
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -15,14 +16,14 @@ function Landing(){
                 </div>
             <img src={landingImg} alt="Proffy" className="hero-image"/>
             <div className="buttons-container">
-                <a href="" className="study">
+                <Link to="/study" className="study">
                     <img src={studyIcon} alt="Estudar"/>
                     Estudar
-                </a>
-                <a href="" className="give-classes">
+                </Link>
+                <Link to="/give-classes" className="give-classes">
                     <img src={giveClassIcon} alt="Dar aulas"/>
                     Dar aulas
-                </a>
+                </Link>
                </div>
                <span className="total-connections">
                 Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="coração roxo" />
