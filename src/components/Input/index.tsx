b1ec69f1f5202ebe02e6f3b03eab810e0b463342
iarademiranda <62669057+iarademiranda@users.noreply.github.com>
2020-08-09 23:31:42 -0300

    teacher  form initial

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input(){
+    return(
+        <div className="input-block">
+            <label htmlFor="subject"></label>
+            <input type="text" id="subject"/>
+        </div>
+    )
+}
+
+export default Input;
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader';
+import './styles.css'
 
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
            <PageHeader title="Que incrível que você quer dar aulas" />
-       </div>
+      
+
+       <main>
+           <fieldset>
+               <legend>Seus dados</legend>
+               <div className="input-block">
+                   <label htmlFor="name">Nome completo</label>
+                   <input type="text" id="name"/>
+               </div>
+
+             
+               <div className="input-block">
+                   <label htmlFor="avatar">Avatar</label>
+                   <input type="text" id="avatar"/>
+               </div>
+
+               <div className="input-block">
+                   <label htmlFor="whatsapp">WhatsApp</label>
+                   <input type="text" id="nawhatsapp"/>
+               </div>
+           </fieldset>
+       </main>
+    </div>
     )
 }
 
